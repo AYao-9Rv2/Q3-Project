e684a38ab006b27343d6163b60725a5ea5bcb9f6
DEvangelista-9R <evangelista.db@obmontessori.edu.ph>
2025-03-07 12:38:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+      function checkPrice() {
+            let seat = document.getElementById('seat').value;
+            let option = document.getElementById('option').value;
+            let price = 0;
 
+            if (seat === "VIP") price = 1000;
+            else if (seat === "Patron") price = 700;
+            else if (seat === "Balcony") price = 500;
+
+            if (option === "Student Discount") price *= 0.8;
+
+            alert("DATA RECEIVED: Your ticket costs PHP " + price + ".");
+        }
